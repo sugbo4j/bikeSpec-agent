766d5ce1ecd59a6942405f18179ac771cb58854c
Joseph Gonzales <josephgonzales01@gmail.com>
2025-06-30 23:44:51 +1200

    Initial commit

---
+++ src/main/java/nz/co/sugbo4j/bikeSpec_agent/BikeSpecAgentApplication.java
@@ -0,0 +1,13 @@
+package nz.co.sugbo4j.bikeSpec_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeSpecAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeSpecAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nz/co/sugbo4j/bikeSpec_agent/BikeSpecAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package nz.co.sugbo4j.bikeSpec_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeSpecAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
